6c5fcff2f53d63913f34db2249abc6c19be18179
Mamady Sillah <supurpisces310@msn.com>
2019-12-04 13:21:24 -0500

    added card data list to scripts page

---
+++ script.js
@@ -0,0 +1,14 @@
+//Create questions and answers for the front and back of the cards
+var cardDataList = [
+ {
+     front:"this is the front of one",
+     back:"this is the back of one",
+ },
+ {   front:"this is the front of two",
+     back:"this is teh back of two",
+ },
+
+]
+
+
+
